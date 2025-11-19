4d1f367647262c04219176e41148807cfef51fc9
Guia <guia.sevilla.gs@gmail.com>
2025-11-19 17:31:35 +0100

    solve lab

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (hacker1.length > hacker2.length) {
 // Iteration 3: Loops
 let nameSpaced = "";
 for (let i = 0; i < hacker1.length; i++) {
-  nameSpaced += hacker1[i].toUpperCase() + " ";
+  nameSpaced += hacker1[i].toUpperCase() + (i < hacker1.length - 1 ? " " : "");
 }
 console.log(nameSpaced);
 
@@ -24,3 +24,31 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
   nameReversed += hacker2[i];
 }  
 console.log(nameReversed);
+
+if (hacker1 < hacker2) {
+    console.log("The driver's name goes first.");
+} else if (hacker2 < hacker1) {
+    console.log("Yo, the navigator goes first, definitely.");
+}else {
+    console.log("What?! You both have the same name?")
+}
+
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie lacus ut leo consectetur dignissim. Donec vitae lectus et nisi convallis hendrerit. Quisque id maximus nulla. Ut volutpat, dui at iaculis ornare, tortor lectus dapibus velit, in facilisis nunc massa sit amet enim. Vestibulum quis nisl leo. Aliquam erat volutpat. Praesent mattis malesuada eros, sit amet tincidunt dui semper at.Aliquam tincidunt lorem ut velit facilisis, sit amet posuere lectus blandit. Morbi molestie nunc id dolor varius, eu luctus nisi porttitor. Ut viverra quam sed blandit dignissim. Sed sit amet arcu a purus luctus lobortis. Donec mollis arcu suscipit tellus dapibus, ac eleifend lorem pulvinar. Aenean eget ultricies odio, ac mattis nisl. Etiam nec turpis vitae lacus sagittis malesuada. Curabitur dolor neque, maximus vitae nibh id, sollicitudin hendrerit ex. Phasellus consequat efficitur lorem a ullamcorper."
+let count = 0;
+for (let i=0; i<=longText.length; i++){
+    if(longText[i] === " "){
+         count ++;
+    }
+    }
+console.log("Words " + count );
+
+
+let etPhoneHome = 0;
+
+for (let i = 0; i < longText.length; i++) {
+    if (longText.substring(i, i + 4) === " et ") {
+        etPhoneHome++;
+    }
+}
+console.log(etPhoneHome);
+
